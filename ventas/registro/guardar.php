bc0e1f8baddaea975aaa542d0b447eb911607fb5
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2013-12-29 22:26:19 -0400

    Grilla en modo Venta

--- ventas/registro/guardar.php
+++ ventas/registro/guardar.php
@@ -17,46 +17,54 @@ if(($_FILES['curriculum']['type']=="application/pdf" || $_FILES['curriculum']['t
 	$mensaje[]="Archivo no válido del curriculum. Verifique e intente nuevamente";
 }
 */
-$cantidadventatotal=$cantidad;
-$fecha=date("Y-m-d");
-$totalproducto=0;
-foreach($compra->mostrarTodo("codproductos=$codproductos and cantidadstock>0","fechacompra") as $inv){
-	$totalproducto+=$inv['cantidadstock'];
-}
-//echo $totalproducto;
-if($totalproducto<$cantidad){
-	$mensaje[]="No Existe en Inventario la Cantidad que Solicita<hr><strong>Cantidad de Inventario: $totalproducto<br>Cantidad de Solicitada: $cantidad</strong>";
-}else{
+/*echo "<pre>";
+print_r($_POST);
+echo "</pre>";*/
+foreach($m as $ma){
+	extract($ma);
+	$cantidadventatotal=$cantidad;
+	$fecha=date("Y-m-d");
+	$totalproducto=0;
 	foreach($compra->mostrarTodo("codproductos=$codproductos and cantidadstock>0","fechacompra") as $inv){
-		if((float)$cantidad<=(float)$inv['cantidadstock']){
-			//echo "si";
-			$mensaje[]="Las Ventas de sus PRODUCTOS SE REGISTRO CORRECTAMENTE";
-			$cantidad=$inv['cantidadstock']-$cantidad;
-			$valores=array("cantidadstock"=>"$cantidad","fechaventa"=>"'$fecha'");
-			$compra->actualizar($valores,$inv["codcompra"]);
-			
-			$valores=array(	"fechaventa"=>"'$fechaventa'",
-				"codproductos"=>"'$codproductos'",
-				"cantidad"=>"'$cantidadventatotal'",
-				"preciounitario"=>"'$preciounitario'",
-				"total"=>"'$total'",
-				"codcliente"=>"'$codcliente'",
-				"coddistribuidor"=>"'$coddistribuidor'",
-				"codvendedor"=>"'$codvendedor'",
-				"observacion"=>"'$observacion'",
-				);
-			$venta->insertar($valores);
-
-			break;	
-		}else{
-			//echo $cantidadsalida;
-			$cantidad=$cantidad-$inv['cantidadstock'];
-			$valores=array("cantidadstock"=>0,"fechaventa"=>"'$fecha'");
-			$compra->actualizar($valores,$inv["codcompra"]);
+		$totalproducto+=$inv['cantidadstock'];
+	}
+	
+	//echo $totalproducto;
+	if($totalproducto<$cantidad){
+		$mensaje[]="No Existe en Inventario la Cantidad que Solicita<hr><strong>Cantidad de Inventario: $totalproducto<br>Cantidad de Solicitada: $cantidad</strong>";
+	}else{
+		foreach($compra->mostrarTodo("codproductos=$codproductos and cantidadstock>0","fechacompra") as $inv){
+			if((float)$cantidad<=(float)$inv['cantidadstock']){
+				//echo "si";
+				$mensaje[]="Las Ventas de sus PRODUCTOS SE REGISTRO CORRECTAMENTE";
+				$cantidad=$inv['cantidadstock']-$cantidad;
+				$valores=array("cantidadstock"=>"$cantidad","fechaventa"=>"'$fecha'");
+				$compra->actualizar($valores,$inv["codcompra"]);
+				
+				$valores=array(	"fechaventa"=>"'$fechaventa'",
+					"codproductos"=>"'$codproductos'",
+					"cantidad"=>"'$cantidadventatotal'",
+					"preciounitario"=>"'$preciounitario'",
+					"total"=>"'$total'",
+					"codcliente"=>"'$codcliente'",
+					"coddistribuidor"=>"'$coddistribuidor'",
+					"codvendedor"=>"'$codvendedor'",
+					"observacion"=>"'$observacion'",
+					);
+				$venta->insertar($valores);
+	/*echo "<pre>";
+print_r($valores);
+echo "</pre>";*/
+				break;	
+			}else{
+				//echo $cantidadsalida;
+				$cantidad=$cantidad-$inv['cantidadstock'];
+				$valores=array("cantidadstock"=>0,"fechaventa"=>"'$fecha'");
+				$compra->actualizar($valores,$inv["codcompra"]);
+			}
 		}
 	}
-}
-	
+}//Fin Foreach
 
 //$mensaje[]="SUS DATOS SE GUARDARON CORRECTAMENTE";
 
--- ventas/registro/nuevo.php
+++ ventas/registro/nuevo.php
@@ -2,80 +2,64 @@
 include_once '../../login/check.php';
 $folder="../../";
 $titulo="Registro de Venta";
-include_once("../../class/compra.php");
-$compra=new compra;
-include_once("../../class/productos.php");
-$productos=new productos;
-$pro=todolista($productos->mostrarTodo("","nombre"),"codproductos","nombre","");
 
-include_once("../../class/distribuidor.php");
-$distribuidor=new distribuidor;
-$dist=todolista($distribuidor->mostrarTodo("","nombre"),"coddistribuidor","nombre,departamento","-");
-
-include_once("../../class/cliente.php");
-$cliente=new cliente;
-$cli=todolista($cliente->mostrarTodo("","nombre"),"codcliente","nombre","-");
-
-include_once("../../class/vendedor.php");
-$vendedor=new vendedor;
-$ven=todolista($vendedor->mostrarTodo("","nombre"),"codvendedor","nombre","-");
 
 include_once '../../funciones/funciones.php';
 include_once '../../cabecerahtml.php';
 ?>
 <script language="javascript">
+var linea=0
+var totalt=0;
 $(document).on("ready",function(){
-	$("#cantidad,#preciounitario").change(function(e){
-		var cantidad=$("#cantidad").val();
-		var preciounitario=$("#preciounitario").val();
+	
+	$(document).on("change",".cantidad,.preciounitario",function(e){
+		var lin=$(this).attr("rel");
+		var cantidad=$(".cantidad[rel="+lin+"]").val();
+		var preciounitario=$(".preciounitario[rel="+lin+"]").val();
 		var total=(cantidad*preciounitario).toFixed(2);
-		$("#total").val(total);
+		$(".total[rel="+lin+"]").val(total);
+		totalt=0;
+		$(".total").each(function(index, element) {
+            totalt+=parseFloat($(this).val());
+			//alert(totalt);
+        });
+		$("#tt").val(totalt.toFixed(2));
 	});	
+	$(document).on("change",".total",function(e){
+		alert("asd");
+	});	
+	function aumentar(){
+
+		$.post("aumentar.php",{l:linea},function(data){
+			$("#marca").before(data);
+			$('input[type=date]').click(function(e){e.preventDefault();}).datepicker();
+			$("select").css({'width':'100%'}).not(".nolista").select2({'placeholder':'Búsqueda no encontrada','loadMorePadding':0});
+			linea++;
+		});
+	}
+	linea++;
+	aumentar();
+	$("#aumentar").click(function(e) {
+		e.preventDefault();
+        aumentar();
+    });
+	$(document).on("change",".productos",function(){
+		var lin=$(this).attr("rel");
+		var can=$(".productos[rel="+lin+"]>option:selected").attr("rel");
+		$(".cantidad[rel="+lin+"]").attr({'max':can});
+	});
 });
 </script>
 <?php include_once '../../cabecera.php';?>
 <div class="grid_12">
 	<div class="contenido">
-    	<div class="prefix_3 grid_4 alpha">
+    	<div class="prefix_0 grid_11 alpha">
 			<fieldset>
 				<div class="titulo"><?php echo $titulo?></div>
                 <form action="guardar.php" method="post" enctype="multipart/form-data">
 				<table class="tablareg">
-                	<tr>
-						<td><?php campos("Fecha de Venta","fechaventa","date",date("Y-m-d"),0,array("required"=>"required"));?></td>
-					</tr>
-					<tr>
-						<td><select name="codproductos" rel-linea="1" class="productos p">									
-                	<option value="">Seleccionar</option>
-                	<?php foreach($productos->mostrarTodo("","nombre") as $pr){$i++;
-					$sum=array_shift($compra->sumar($pr['codproductos']));
-					$sumatotal=$sum['cantidadtotalstock']!=""?$sum['cantidadtotalstock']:'0';?>
-                    <option value="<?php echo $pr['codproductos']?>" rel="<?php echo $sumatotal;?>"><?php echo $pr['nombre']." - Cantidad Stock: ".$sumatotal;?></option>
-                    <?php }?>
-                </select></td>
-					</tr>
-					<tr>
-						<td><?php campos("Cantidad","cantidad","number","0",0,array("class"=>"der","min"=>0));?></td>
-					</tr>
-                    <tr>
-						<td><?php campos("Precio Unitario","preciounitario","number","0.00",0,array("step"=>"0.1","min"=>0,"class"=>"der"));?>Bs</td>
-					</tr>
-                    <tr>
-						<td><?php campos("Total","total","text","0.00",0,array("class"=>"der","readonly"=>"readonly"));?>Bs</td>
-					</tr>
-                    <tr>
-						<td><?php campos("Cliente","codcliente","select",$cli);?></td>
-					</tr>
-                    <tr>
-						<td><?php campos("Distribuidor","coddistribuidor","select",$dist);?></td>
-					</tr>
-                    <tr>
-						<td><?php campos("Vendedor","codvendedor","select",$ven);?></td>
-					</tr>
-                    <tr>
-						<td><?php campos("Observación","observacion","textarea");?></td>
-					</tr>
-					<tr><td><div class="rojoC pequeno">La Cantidad Introducida se utilizará para descontar del inventario, Revíselo antes de Registrarlo, Posteriormente no se podra modificar la CANTIDAD y PRECIO de venta</div><?php campos("Guardar","guardar","submit");?></td><td></td></tr>
+                	<tr class="titulo"><td>N</td><td>Fecha de Venta</td><td width="450" style="width:450px !important">Producto<hr class="separador">Cliente</td><td>Cantidad<hr class="separador">Distribuidor</td><td>Precio Unitario<hr class="separador">Vendedor</td><td>Total<hr class="separador">Observaciones</td></tr>
+					<tr id="marca"><td colspan="2" class="subir"><a href="#" id="aumentar">Aumentar</a></td><td colspan="3"><div class="rojoC pequeno">La Cantidad Introducida se utilizará para descontar del inventario, Revíselo antes de Registrarlo, Posteriormente no se podra modificar la CANTIDAD y PRECIO de venta</div><?php campos("Guardar","guardar","submit");?></td><td class="subir"><input type="text" value="0.00" class="der" style="width:80px;" readonly id="tt" name="tt"></td></tr>
 				</table>
                 </form>
 			</fieldset>
