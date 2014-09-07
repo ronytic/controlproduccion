2733ace1a992619ed3e065a82798936b5ef9645d
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-09-07 13:44:19 -0400

    Corregido Envasado

--- class/envasado.php
+++ class/envasado.php
@@ -3,8 +3,12 @@ include_once("bd.php");
 class envasado extends bd{
 	var $tabla="envasado";
 	function sumarProducto($codproductos){
-		$this->campos=array("sum(cantidad) as cantidadventatotal");
+		$this->campos=array("sum(cantidadstock) as cantidadventatotal");
 		return $this->getRecords("codproductos=$codproductos and activo=1");	
+	}
+	function agrupado(){
+		$this->campos=array("sum(cantidad) as cantidadtotal,codenvase,codproductos,codcompra");
+		return $this->getRecords("activo=1","","codproductos,codenvase");	
 	}
 }
 ?>
