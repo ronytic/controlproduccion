3b29edf7a150b55a395d4d59077dbe3e68cdf563
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-12-23 21:55:02 -0400

    Añadido Clases de Base de Datos

---
+++ class/ventacredito.php
@@ -0,0 +1,7 @@
+<?php
+include_once("bd.php");
+class ventacredito extends bd{
+	var $tabla="ventacredito";
+	
+}
+?>
---
+++ class/ventageneral.php
@@ -0,0 +1,7 @@
+<?php
+include_once("bd.php");
+class ventageneral extends bd{
+	var $tabla="ventageneral";
+	
+}
+?>
